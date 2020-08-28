9159859966303aa53369a9b90978acda9b9a18a3
Nick <nick@networkbasedsolutions.com>
2020-08-28 15:25:22 -0700

    Added code move duplicate files into a 'dups' directory.
    * Fixed a bug where non-files where being processed.
    * Refactored code into smaller functions.
    * Added code to create the 'dups' directory if it doesn't exist.
    * Flipped the logic for determining if a file is a duplicate, making it easier to understand.
    * Added basic logging.

--- src/scripts/app.ts
+++ src/scripts/app.ts
@@ -29,8 +29,12 @@ async function fetchFileHashes(directory: string): Promise<FileHash[]> {
 
     const files = fs.readdirSync(directory);
     files.forEach(file => {
-        const result = processFile(`${directory}/${file}`);
-        promises.push(result);
+        let filePath = `${directory}/${file}`;
+
+        if (fs.lstatSync(filePath).isFile()) {
+            const result = processFile(filePath);
+            promises.push(result);
+        }
     })
 
     return Promise.all(promises);
@@ -40,8 +44,25 @@ function fileHashExists(hash: string, fileHashes: FileHash[]): boolean {
     return fileHashes.findIndex(f => f.hash === hash) !== -1;
 }
 
+function getFilename(filePath: string): string {
+    return filePath.split("/").pop();
+}
+
+function moveDuplicate(fileHash: FileHash, destination: string) {
+    let oldFilePath = fileHash.file;
+    let newFilePath = `${destination}/${getFilename(fileHash.file)}`;
+    fs.renameSync(oldFilePath, newFilePath);
+
+    console.log(`Moved the duplicate file: ${oldFilePath} to ${newFilePath}.`);
+}
+
 const args = process.argv.slice(2);
 const srcDir = args[0];
+const dstDir = `${srcDir}/dups`;
+
+if (!fs.existsSync(dstDir)) {
+    fs.mkdirSync(dstDir);
+}
 
 (async () => {
     const unique: FileHash[] = [];
@@ -50,13 +71,14 @@ const srcDir = args[0];
     const md5Files = await fetchFileHashes(srcDir);
 
     md5Files.forEach(fileHash => {
-        if (!fileHashExists(fileHash.hash, unique)) {
-            unique.push(fileHash);
-        } else {
+        if (fileHashExists(fileHash.hash, unique)) {
             duplicate.push(fileHash);
+            moveDuplicate(fileHash, dstDir);
+        } else {
+            unique.push(fileHash);
         }
     })
 
-    console.log(duplicate.length);
-    console.log(duplicate);
+    console.log(`Processed ${md5Files.length} file(s).`);
+    console.log(`Moved ${duplicate.length} duplicate file(s).`);
 })();
